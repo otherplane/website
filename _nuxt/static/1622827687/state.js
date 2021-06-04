7586756290ec7124ed0e8b86de4801459034ae1c
tommytrg <tomastrg22@gmail.com>
2021-06-04 19:28:07 +0200

    deploy

---
+++ _nuxt/static/1622827687/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwebsite\u002F_nuxt\u002Fstatic\u002F1622827687",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fwebsite\u002F",assetsPath:"\u002Fwebsite\u002F_nuxt\u002F",cdnURL:a}}}}(null));
